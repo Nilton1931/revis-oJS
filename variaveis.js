6bc9608d80184ca4f1d921c47d5d6d2f6f2c7cfb
Nilton1931 <jsreijose10@gmail.com>
2025-03-25 19:04:19 +0000

    era pra salvar

---
+++ variaveis.js
@@ -0,0 +1,51 @@
+//var A = 5
+//var B = 10
+
+//var C = A + B
+
+
+
+
+
+
+//var nome = "Nilton"
+//console.log(nome)
+
+//let nome = "nilton"
+//nome = "azul"
+
+
+
+
+//const nome = (Cinza)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var nome = "Nilton" 
+let idade = 18
+const signo = "virgem"
+
+/* 
+var é que não altera o resultado do código, além de ser um comando "global", e antigo.
+let pode ser mudado com o passar do tempo, exp: idade
+const não pode ser mudado com o passar do tempo, exp: nome, cpf, signo,etc...
+ */
